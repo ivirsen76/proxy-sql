93d1646bca12b5014f49305a16643a478da0b004
Igor Eremeev <eremeev@3cisd.com>
2019-11-06 23:39:32 -0500

    Allow collapsing navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,29 @@
 import React from "react";
-import NavLink from './NavLink'
+import NavLink from "./NavLink";
 import { Link } from "react-router-dom";
 
 export default props => {
     return (
-        <nav className="navbar navbar-expand navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <div className="container">
                 <Link className="navbar-brand" to="/">
                     Home
                 </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
 
                 <div
                     className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
+                    id="navbarNavAltMarkup"
                 >
                     <ul className="navbar-nav mr-auto">
                         <NavLink to="/settings">Settings</NavLink>
