213b567524db3c784a0d9afa9a8d145bcded3e83
Igor Eremeev <eremeev@3cisd.com>
2019-11-08 10:32:59 -0500

    Persist redux store

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from "react";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
 import Words from "./pages/Words";
 import Mode from "./pages/Mode";
-import { Provider } from 'react-redux'
-import store from './store.js'
+import { Provider } from "react-redux";
+import { store, persistor } from "./store.js";
 import style from "./App.module.css";
 
 export default () => {
     return (
         <Provider store={store}>
-            <Router>
-                <Nav />
-                <div className={"container " + style.body}>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/settings" component={Settings} />
-                        <Route exact path="/words" component={Words} />
-                        <Route exact path="/mode" component={Mode} />
-                    </Switch>
-                </div>
-            </Router>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <Nav />
+                    <div className={"container " + style.body}>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route
+                                exact
+                                path="/settings"
+                                component={Settings}
+                            />
+                            <Route exact path="/words" component={Words} />
+                            <Route exact path="/mode" component={Mode} />
+                        </Switch>
+                    </div>
+                </Router>
+            </PersistGate>
         </Provider>
     );
 };
--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
-import { createStore, combineReducers } from 'redux'
-import settingsReducer from './reducers/settings.js'
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import reducer from "./reducers/main.js";
 
-const rootReducer = combineReducers({
-  settings: settingsReducer,
-})
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, reducer);
 
-export default createStore(rootReducer)
+export const store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export const persistor = persistStore(store);
